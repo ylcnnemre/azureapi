8dbd2c1fac689c77ef0aee4c6c466000451e240e
emreylc <yalcnnemre@gmail.com>
2023-07-03 19:40:25 +0300

    some changesss

--- app.js
+++ app.js
@@ -17,4 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+app.listen(3000,()=>{
+    console.log("server is runnning")
+})
