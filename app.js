91d01ecc0cd1fc47ee700e70b991be12971ae0db
emreylc <yalcnnemre@gmail.com>
2023-07-03 19:25:55 +0300

    commit test 12

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const express=require("express")
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app=express()
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
+var app = express();
 
-app.get("/test",(req,res)=>{
-    res.send("selammmm")
-})
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-
-app.listen(3000,()=>{
-    console.log("server is running")
-})
+module.exports = app;
